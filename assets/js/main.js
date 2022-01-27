44ceafbc0d1f7ece589735361c3a2c9e82e51558
Cahyo <48130708+cahyo19@users.noreply.github.com>
2022-01-27 10:29:38 +0700

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,3 +123,11 @@ resumeButton.addEventListener('click', () => {
     setTimeout(removeScale, 5000)
 })
 
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker
+            .register("/serviceWorker.js")
+            .then(res => console.log("service worker registered"))
+            .catch(err => console.log("service worker not registered", err))
+    })
+}
